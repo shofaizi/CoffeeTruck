c4d8fc6532b023166452dc735c8468df26e96f4d
Sho <sfaizi1@outlook.com>
2017-01-19 18:49:30 -0800

    registering createOrder as submit handler

--- scripts/formhandler.js
+++ scripts/formhandler.js
@@ -11,24 +11,28 @@
     if(this.$formElement.length === 0) {
       throw new Error('Could not find element with selector: ' + selector);
     }
-  }
+  };
 
-  FormHandler.prototype.addSubmitHandler = function() {
+  FormHandler.prototype.addSubmitHandler = function(fn) {
+    // fn is used to access truck instance's createOrder method as a callback
     console.log('Setting submit handler for form');
-    this.$formElement.on('submit', function(e) {
-      e.preventDefault();
+    this.$formElement.on('submit', function(event) {
+      event.preventDefault();
 
       var data = {};
       $(this).serializeArray().forEach(function(item){
         //serializeArray gets data from form
         data[item.name] = item.value;
+        //placing each item with its value into data object
         console.log(item.name + " is " + item.value);
       });
       console.log(data);
+      fn(data); //callback is invoked and will pass user's data
+      this.reset();
+      this.elements[0].focus();
     });
-  }
+  };
 
   App.FormHandler = FormHandler;
   window.App = App;
-
 })(window);
