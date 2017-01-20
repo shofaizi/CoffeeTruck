835f4c496ccc7771ec24794394cb5316d9369621
Sho <sfaizi1@outlook.com>
2017-01-20 11:16:15 -0800

    add Row cosntructor to ckecklist.js and create checklist items append to DOM

--- scripts/checklist.js
+++ scripts/checklist.js
@@ -1,4 +1,8 @@
 (function(windown) {
+  // this module will have three methods:
+  // 1. create a checklist item, including the checkbox and the text description
+  // 2. add event listener to remove row
+  // 3. remove the row of checklist item
   'use strict';
   var App = window.App || {};
   var $ = window.jQuery;
@@ -12,6 +16,34 @@
       throw new Error('Could not find element with selector ' + selector);
     }
   }
+
+  function Row(coffeeOrder) {
+    var $div = $('<div></div>', {
+      'data-coffee-order': 'checkbox',
+      'class': 'checkbox'
+    });
+
+    var $label = $('<label></label>', {
+      type: 'checkbox',
+      value: coffeeOrder.emailAddress
+    });
+
+    var description = coffeeOrder.size + ' ';
+    if(coffeeOrder.flavor) {
+      description += coffeeOrder.flavor + ' ';
+    }
+
+    description += coffeeOrder.coffee + ', ';
+    description += ' (' + coffeeOrder.emailAddress + ')';
+    description += ' [' + coffeeOrder.strength + 'x]';
+
+    $label.append($checkbox);
+    $label.append(description);
+    $div.append($label);
+
+    this.$element = $div;
+  }
+
   App.Checklist = Checklist;
   window.App = App;
 })(window);
